d3d586bb35c9d06bd70f1eed3a5c20dfdf164b4d
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-06-23 22:25:40 +0300

    fix : responsive fixed

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 function Navigation() {
   return (<>
-     <nav className='d-flex justify-content-center py-3 shadow-sm p-3 mb-5'>
-        <ul className="nav">
+     <nav className='py-3 shadow-sm p-3 mb-4'>
+        <ul className="nav container-fluid d-flex justify-content-center">
             <li className="nav-item">
-                <a className="nav-link text-light fs-6" aria-current="page" href="#!"> <i class="bi bi-house"></i> Accueil</a>
+                <a className="nav-link text-light fs-6" aria-current="page" href="#!"> <i className="bi bi-house"></i> Accueil</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-light fs-6" href="#!"> <i class="bi bi-map"></i> Voyage</a>
+                <a className="nav-link text-light fs-6" href="#!"> <i className="bi bi-map"></i> Voyage</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-light fs-6" href="#!"> <i class="bi bi-bookmark"></i> Réservation</a>
+                <a className="nav-link text-light fs-6" href="#!"> <i className="bi bi-bookmark"></i> Réservation</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-light fs-6" href="#!"> <i class="bi bi-person-workspace"></i> Services</a>
+                <a className="nav-link text-light fs-6" href="#!"> <i className="bi bi-person-workspace"></i> Services</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link text-light fs-6" href="#!"> <i class="bi bi-telephone"></i> Infoline</a>
+                <a className="nav-link text-light fs-6" href="#!"> <i className="bi bi-telephone"></i> Infoline</a>
             </li>
         </ul>
     </nav>
