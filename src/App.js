74af50cfc79f910b3a6817f1a165e993e07435ca
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-06-23 22:26:53 +0300

    feat : intro line added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from "./Components/Footer";
 function App() {
   return (<>
     < Navigation/>
+    <blockquote>Bienvenue sur Toky Transport : la coopérative du futur</blockquote>
+    <p className="recap">Voici les listes des villes de destination, nos véhicules et nos tarifs.</p>
     < Section />
     <Footer />
   </>);
