ea193bef42c12e34bf89947f4af1b53126df9462
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-06-23 20:34:20 +0300

    fix : class changed to className in Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,19 +3,19 @@ import React from 'react'
 function Footer() {
   return (<>
 
-<footer class="text-center text-lg-start mt-4 text-white">
-  <div class="container p-4">
-      <div class="text-center">
-        <h5 class="text-uppercase">Contacts</h5>
-        <p> <i class="bi bi-geo-alt"></i> Adresse : Ivandry, Antananarivo 101 <br/>
-        <i class="bi bi-envelope"></i> Email : yourtransport@fiara.mg <br/>
-          <i class="bi bi-telephone"></i> Tél : +261 38 574 23
+<footer className="text-center text-lg-start mt-4 text-white">
+  <div className="container p-4">
+      <div className="text-center">
+        <h5 className="text-uppercase">Contacts</h5>
+        <p> <i className="bi bi-geo-alt"></i> Adresse : Ivandry, Antananarivo 101 <br/>
+        <i className="bi bi-envelope"></i> Email : yourtransport@fiara.mg <br/>
+          <i className="bi bi-telephone"></i> Tél : +261 38 574 23
         </p>
       </div>
   </div>
-  <div class="text-center p-3" >
+  <div className="text-center p-3" >
     © 2022 Copyright :
-    <a class="text-light text-decoration-none" href='#!'> Groupe 4 (G2) </a>
+    <a className="text-light text-decoration-none" href='#!'> Groupe 4 (G2) </a>
   </div>
 </footer>
 
