1aace7d398b6d96185a6f8f22329f00504985fba
Sanda Mandresy <hei.sanda.3@gmail.com>
2022-06-23 22:26:15 +0300

    feat: data examples added

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -3,9 +3,34 @@ import React from 'react'
 function Section() {
   return (<>
       <div className='d-flex justify-content-evenly'>
-        <span className='rounded-3 w-25'><h2 className='fs-6 rounded-3'>Villes</h2></span>
-        <span className='rounded-3 w-25'><h2 className='fs-6 rounded-3'>Véhicules</h2></span>
-        <span className='rounded-3 w-25'><h2 className='fs-6 rounded-3'>Tarifs</h2></span>
+        <span className='rounded-3 w-25'>
+            <h2 className='fs-6 rounded-3'>Villes</h2>
+            <ol>
+              <li>Antananarivo</li>
+              <li>Majunga</li>
+              <li>Fianarantsoa</li>
+              <li>Tamatave</li>
+              <li>Tuléar</li>
+            </ol>
+        </span>
+        <span className='rounded-3 w-25'>
+          <h2 className='fs-6 rounded-3'>Véhicules</h2>
+          <ol>
+              <li>Mitsubishi</li>
+              <li>Ford</li>
+              <li>Ferrari</li>
+              <li>Hyundai</li>
+              <li>Renault</li>
+            </ol>
+        </span>
+        <span className='rounded-3 w-25'>
+            <h2 className='fs-6 rounded-3'>Tarifs</h2>
+            <ol>
+              <li>300000</li>
+              <li>150000</li>
+              <li>15000</li>
+            </ol>
+        </span>
       </div>
   </>);
 }
